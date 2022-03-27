2f71769c7e547be039af3902a7eaad4f28d9a7ad
Ryan Alameddine <rhalameddin@gmail.com>
2022-03-26 17:05:26 -0700

    Added console clear logic for mac, linux, and windows

--- Application/QtFrontend/MainWindow.cpp
+++ Application/QtFrontend/MainWindow.cpp
@@ -539,7 +539,13 @@ void CMainWindow::LoadEmptyNomeFile()
 
 void CMainWindow::LoadNomeFile(const std::string& filePath)
 {
-    //system("CLS");
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)                     \
+    || defined(__WINDOWS__) || defined(_WIN64)
+    system("CLS");
+#else
+    system("clear");
+#endif
+ 
     printf("Loading file: %s \n\n", filePath.c_str());
     setWindowFilePath(QString::fromStdString(filePath));
     bIsBlankFile = false;
